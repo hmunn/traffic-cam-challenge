a3a76279bc82c083f7fdd1b57ab5c544cdd8abab
Dave Stearns <dlsinfo@uw.edu>
2014-10-04 08:27:50 -0700

    file stubs and assets

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// List of Seattle Traffic Cameras
+// http://data.seattle.gov/resource/65fc-btcc.json
+
+"use strict";
+
+//put your code here to create the map, fetch the list of traffic cameras
+//and add them as markers on the map
+//when a user clicks on a marker, you should pan the map so that the marker
+//is at the center, and open an InfoWindow that displays the latest camera
+//image
+//you should also write the code to filter the set of markers when the user
+//types a search phrase into the search box
+
